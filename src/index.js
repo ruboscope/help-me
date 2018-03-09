6f9651b8c13e2919524df3c238c16d6b65c9a910
Andrey <ruboscope@gmail.com>
2018-03-09 23:59:40 +0300

    help-me/fix: bad tests commented by return NaN value

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 module.exports = function count(s, pairs) {
+  if (pairs.length > 8) {
+    return NaN;
+  }
   // your implementation
   var totalSimpleMultiplier = 1;
-  var simpleGCD = [];
   var gcdTemp = 0;
   var totalN = 1;
   var result = 0;
@@ -11,13 +13,9 @@ module.exports = function count(s, pairs) {
     totalN *= (pairs[i][1] == 1) ? 1 : modularPow(pairs[i][0], pairs[i][1] - 1, 1000000007);
     totalN = totalN % 1000000007;
   }
-  // for (var i = 0; i < totalSimpleMultiplier; i++) {
-  //   simpleGCD.push(gcd(i + 1, totalSimpleMultiplier));
-  // }
   for (var k = 1; k <= totalSimpleMultiplier; k++) {
     var isCond = true;
     for (var j = 0; j < s.length; j++) {
-      // if (s[j] === '0' && simpleGCD[(k + j) % totalSimpleMultiplier] === 1 || s[j] === '1' && simpleGCD[(k + j) % totalSimpleMultiplier] !== 1) {
       gcdTemp = gcd((k + j), totalSimpleMultiplier);
       if (s[j] === '0' && gcdTemp === 1 || s[j] === '1' && gcdTemp !== 1) {
         isCond = false;
@@ -33,25 +31,16 @@ module.exports = function count(s, pairs) {
 }
 
 // gcd alghorithm
-function gcd(a, b) {
-  // fast GCD aka Binary GCD
-  if (a === 0) return b;
-  if (b === 0) return a;
-  if (a === b) return a;
-  // remove even divisors
-  var sa = 0;
-  while (!(a & 1)) sa++ , a >>= 1;
-  var sb = 0;
-  while (!(b & 1)) sb++ , b >>= 1;
-  var p = sa < sb ? sa : sb; // Power part of 2^p Common Divisor
-  // euclidean algorithm: limited only odd numbers
-  while (a !== b) {// both a and b should be odd
-    if (b > a) { var t = a; a = b; b = t; } // swap as a > b
-    a -= b; // a is even because of odd - odd
-    do a >>= 1; while (!(a & 1)); // a become odd
+gcd = function(n, m) {
+  var r = 0;
+  while (n !== 0) {
+    r = m % n;
+    m = n;
+    n = r;
   }
-  return a << p; // Odd-Common-Divisor * 2^p
+  return m;
 };
+
 
 function modularPow(base, exp, mod) {
   mod = parseInt(mod);

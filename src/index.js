742501a1794ca6228b00e8298cce335c6248efd3
Andrey <ruboscope@gmail.com>
2018-03-14 23:06:21 +0300

    help-me/fix: 2 and 14 test is completed

--- src/index.js
+++ src/index.js
@@ -1,37 +1,51 @@
-module.exports = function count(s, pairs) {
-  if (pairs.length > 7) {
+module.exports = module.exports = function count(s, pairs) {
+  if (pairs.length > 7 && s.length > 1) {
     return NaN;
   }
   // your implementation
   var totalSimpleMultiplier = 1;
   var gcdTemp = 0;
   var totalN = 1;
-  var result = 0;
+  var result = 1;
+
+
+  /* solution throught gcd*/
   debugger;
   for (var i = 0; i < pairs.length; i++) {
-    totalSimpleMultiplier *= pairs[i][0];
+    result = multiply(result, (pairs[i][0] - 1));
+    totalSimpleMultiplier = multiply(totalSimpleMultiplier, pairs[i][0]);
     totalN *= (pairs[i][1] == 1) ? 1 : modularPow(pairs[i][0], pairs[i][1] - 1, 1000000007);
     totalN = totalN % 1000000007;
   }
-  for (var k = 1; k <= totalSimpleMultiplier; k++) {
-    var isCond = true;
-    for (var j = 0; j < s.length; j++) {
-      gcdTemp = gcd((k + j), totalSimpleMultiplier);
-      if (s[j] === '0' && gcdTemp === 1 || s[j] === '1' && gcdTemp !== 1) {
-        isCond = false;
-        break;
-      }
-    }
-    if (isCond) {
-      result++;
+  if (s.length == 1) {
+    if (s[0] == '0') {
+      result = totalSimpleMultiplier - result;
     }
   }
+  else {
+    result = 0;
+    for (var k = 1; k <= totalSimpleMultiplier; k++) {
+      var isCond = true;
+      for (var j = 0; j < s.length; j++) {
+        gcdTemp = gcd((k + j), totalSimpleMultiplier);
+        if (s[j] === '0' && gcdTemp === 1 || s[j] === '1' && gcdTemp !== 1) {
+          isCond = false;
+          break;
+        }
+      }
+      if (isCond) {
+        result++;
+      }
+    }
+  }
+/*end gcd solution */
+
   result *= totalN;
   return result % 1000000007;
 }
 
 // gcd alghorithm
-gcd = function(n, m) {
+gcd = function (n, m) {
   var r = 0;
   while (n !== 0) {
     r = m % n;
@@ -42,31 +56,74 @@ gcd = function(n, m) {
 };
 
 
-function modularPow(base, exp, mod) {
-  mod = parseInt(mod);
-  s = 1; v = exp; c = base;
-  while (v != 0) {
-    flag = 0;
-    if (v % 2 == 1) {
-      if (!mod)
-        s = s * c;
-      else
-        s = (s * c) % mod;
-      v = (v - 1) / 2;
-      if (!mod)
-        c = c * c;
-      else
-        c = (c * c) % mod;
-      flag = 1;
-    }
-    else {
-      v = v / 2;
-    }
-    if (!flag)
-      if (!mod)
-        c = c * c;
-      else
-        c = (c * c) % mod;
+function modularPow(base, exp, modular) {
+  var baseMod = base % modular;
+  debugger;
+  if (exp == 0) {
+    return 1;
+  } else if (exp % 2 == 0) {
+    var temp = modularPow(base, exp / 2, modular);
+    temp = temp % modular;
+    return modulo(multiply(temp, temp), modular);
   }
-  return s;
+  else {
+    return (baseMod * modularPow(base, exp - 1, modular) % modular) % modular;
+  }
+}
+function modulo(divident, divisor) {
+  var cDivident = '';
+  var cRest = '';
+
+  for (var i in divident) {
+    var cChar = divident[i];
+    var cOperator = cRest + '' + cDivident + '' + cChar;
+
+    if (cOperator < parseInt(divisor)) {
+      cDivident += '' + cChar;
+    } else {
+      cRest = cOperator % divisor;
+      if (cRest == 0) {
+        cRest = '';
+      }
+      cDivident = '';
+    }
+
+  }
+  cRest += '' + cDivident;
+  if (cRest == '') {
+    cRest = 0;
+  }
+  return cRest;
+}
+function multiply(elem1, elem2) {
+  if ((elem1 | 0) == 0 || (elem2 | 0) == 0) {
+    return '0';
+  }
+
+  elem1 = elem1.toString().split('').reverse();
+  elem2 = elem2.toString().split('').reverse();
+  var result = [];
+
+  for (var i = 0; elem1[i] >= 0; i++) {
+    for (var j = 0; elem2[j] >= 0; j++) {
+      if (!result[i + j]) {
+        result[i + j] = 0;
+      }
+
+      result[i + j] += elem1[i] * elem2[j];
+    }
+  }
+
+  for (var i = 0; result[i] >= 0; i++) {
+    if (result[i] >= 10) {
+      if (!result[i + 1]) {
+        result[i + 1] = 0;
+      }
+
+      result[i + 1] += parseInt(result[i] / 10);
+      result[i] %= 10;
+    }
+  }
+
+  return result.reverse().join('');
 }
